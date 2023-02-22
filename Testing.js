2c7fb4ca9ee9b2da7d917be272842ba5b3d067c8
MishaKollen <mikhailkollen@gmail.com>
2023-02-22 16:57:36 +0400

    Setup Products Context and  Reducer

---
+++ Testing.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+const Testing = () => {
+  return <div>Testing</div>;
+};
+
+const Wrapper = styled.section``;
+
+export default Testing;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
+
+import styled from "styled-components";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="cart" element={<Cart />}></Route>
+        <Route path="products" element={<Products />}></Route>
+        <Route path="products/:id" element={<SingleProduct />}></Route>
+        <Route path="*" element={<Error />}></Route>
+        <Route
+          path="checkout"
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        ></Route>
+      </Routes>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
-export default App
+export default App;
