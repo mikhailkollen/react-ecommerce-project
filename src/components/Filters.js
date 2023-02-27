7e2450f78e6a4c3dad63a8af4aca32b05bd69980
MishaKollen <mikhailkollen@gmail.com>
2023-02-27 17:34:42 +0400

    add filtering functionality

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,154 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useFilterContext } from '../context/filter_context'
-import { getUniqueValues, formatPrice } from '../utils/helpers'
-import { FaCheck } from 'react-icons/fa'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import { getUniqueValues, formatPrice } from "../utils/helpers";
+import { FaCheck } from "react-icons/fa";
 
 const Filters = () => {
-  return <h4>filters</h4>
-}
+  const {
+    filters: {
+      text,
+      category,
+      company,
+      color,
+      min_price,
+      max_price,
+      price,
+      shipping,
+    },
+    updateFilters,
+    clearFilters,
+    all_products,
+  } = useFilterContext();
+
+  const categories = getUniqueValues(all_products, "category");
+  const companies = getUniqueValues(all_products, "company");
+  const colors = getUniqueValues(all_products, "colors");
+
+  return (
+    <Wrapper>
+      <div className="content">
+        <form onSubmit={(e) => e.preventDefault()}>
+          {/* search input */}
+          <div className="form-control">
+            <input
+              type="text"
+              name="text"
+              placeholder="search"
+              className="search-input"
+              value={text}
+              onChange={updateFilters}
+            />
+          </div>
+          {/* end of search input */}
+          {/* categories */}
+          <div className="form-control">
+            <h5>category</h5>
+            <div>
+              {categories.map((c, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={updateFilters}
+                    name="category"
+                    value={c}
+                    className={category === c.toLowerCase() ? "active" : null}
+                  >
+                    {c}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of categories */}
+          {/* companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+              id=""
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* end of companies */}
+          {/* colors */}
+          <div className="form-control">
+            <h5>colors</h5>
+            <div className="colors">
+              {colors.map((c, index) => {
+                if (c === "all") {
+                  return (
+                    <button
+                      key={index}
+                      name="color"
+                      value="all"
+                      onClick={updateFilters}
+                      className={`${color === "all" ? "active" : ""} all-btn`}
+                    >
+                      All
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name="color"
+                    value={c}
+                    style={{ background: c }}
+                    className={`${color === c ? "active" : ""} color-btn`}
+                    onClick={updateFilters}
+                  >
+                    {color === c && <FaCheck />}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
+          {/* price */}
+          <div className="form-control">
+            <h5>price</h5>
+            <p className="price">{formatPrice(price)}</p>
+            <input
+              type="range"
+              name="price"
+              onChange={updateFilters}
+              min={min_price}
+              max={max_price}
+              value={price}
+            />
+          </div>
+          {/* end of price */}
+          {/* shipping */}
+          <div className="form-control shipping">
+            <label htmlFor="shipping">free shipping</label>
+            <input
+              type="checkbox"
+              name="shipping"
+              id="shipping"
+              onChange={updateFilters}
+              checked={shipping}
+            />
+          </div>
+          {/* end of shipping */}
+        </form>
+        <button className="clear-btn" type="button" onClick={clearFilters}>
+          Clear Filters
+        </button>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   .form-control {
@@ -23,6 +165,9 @@ const Wrapper = styled.section`
     letter-spacing: var(--spacing);
   }
   .search-input::placeholder {
+    text-transform: capitalize;
+  }
+  select {
     text-transform: capitalize;
   }
 
@@ -86,13 +231,18 @@ const Wrapper = styled.section`
     margin-bottom: 0.25rem;
   }
   .shipping {
-    display: grid;
+    display: flex;
     grid-template-columns: auto 1fr;
     align-items: center;
     text-transform: capitalize;
-    column-gap: 0.5rem;
+    // column-gap: 0.5rem;
     font-size: 1rem;
     max-width: 200px;
+  }
+  #shipping {
+    margin-left: 1rem;
+    width: 1.2rem;
+    height: 1.2rem;
   }
   .clear-btn {
     background: var(--clr-red-dark);
@@ -106,6 +256,6 @@ const Wrapper = styled.section`
       top: 1rem;
     }
   }
-`
+`;
 
-export default Filters
+export default Filters;
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -12,10 +12,18 @@ import {
 const filter_reducer = (state, action) => {
   switch (action.type) {
     case LOAD_PRODUCTS:
+      let maxPrice = Math.max(...action.payload.map((p) => p.price));
+      let minPrice = Math.min(...action.payload.map((p) => p.price));
       return {
         ...state,
         all_products: [...action.payload],
         filtered_products: [...action.payload],
+        filters: {
+          ...state.filters,
+          max_price: maxPrice,
+          min_price: minPrice,
+          price: maxPrice,
+        },
       };
 
     case SET_GRIDVIEW:
@@ -27,7 +35,7 @@ const filter_reducer = (state, action) => {
     case UPDATE_SORT:
       return { ...state, sort: action.payload };
 
-    case SORT_PRODUCTS:
+    case SORT_PRODUCTS: {
       const { sort, filtered_products } = state;
       let tempProducts = [...filtered_products];
       switch (sort) {
@@ -49,6 +57,60 @@ const filter_reducer = (state, action) => {
           break;
       }
       return { ...state, filtered_products: tempProducts };
+    }
+
+    case UPDATE_FILTERS:
+      const { name, value } = action.payload;
+      return { ...state, filters: { ...state.filters, [name]: value } };
+
+    case FILTER_PRODUCTS:
+      const { all_products } = state;
+      const { text, category, company, color, price, shipping } = state.filters;
+      let tempProducts = [...all_products];
+      if (text) {
+        tempProducts = tempProducts.filter((product) => {
+          return product.name.toLowerCase().includes(text);
+        });
+      }
+      if (category !== "all") {
+        tempProducts = tempProducts.filter(
+          (product) => product.category === category
+        );
+      }
+      if (company !== "all") {
+        tempProducts = tempProducts.filter(
+          (product) => product.company === company
+        );
+      }
+      if (color !== "all") {
+        tempProducts = tempProducts.filter((product) => {
+          return product.colors.find((c) => c === color);
+        });
+      }
+
+      tempProducts = tempProducts.filter((product) => product.price <= price);
+
+      if (shipping) {
+        tempProducts = tempProducts.filter(
+          (product) => product.shipping === true
+        );
+      }
+
+      return { ...state, filtered_products: tempProducts };
+
+    case CLEAR_FILTERS:
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          text: "",
+          company: "all",
+          category: "all",
+          color: "all",
+          price: state.filters.max_price,
+          shipping: false,
+        },
+      };
 
     default:
       throw new Error(`No Matching "${action.type}" - action type`);
